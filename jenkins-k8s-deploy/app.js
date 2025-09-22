db451d48a2faf65a034998fde055570c4f62aaa7
Benny <132187364+Benny-17@users.noreply.github.com>
2025-09-22 14:33:31 +0530

    eks deploy!

---
+++ jenkins-k8s-deploy/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h2>Node.js App Running on EKS 🚀</h2>
+    <p><strong>Environment:</strong> ${process.env.NODE_ENV}</p>
+    <p><strong>App:</strong> ${process.env.APP_NAME}</p>
+    <p><strong>Region:</strong> ${process.env.REGION}</p>
+    <p><strong>API_KEY:</strong> ${process.env.API_KEY}</p>
+  `);
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
