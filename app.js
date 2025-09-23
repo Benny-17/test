98fb16fb54303b686d7bcc788e93bf0449dfdf7e
Benny <132187364+Benny-17@users.noreply.github.com>
2025-09-23 15:17:27 +0530

    Update app.js-01

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send(`
-    <h2>Node.js App Running on EKS 🚀</h2>
+    <h2>EKS</h2>
     <p><strong>Environment:</strong> ${process.env.NODE_ENV}</p>
     <p><strong>App:</strong> ${process.env.APP_NAME}</p>
     <p><strong>Region:</strong> ${process.env.REGION}</p>
@@ -16,3 +16,4 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 });
+
