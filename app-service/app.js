1a046bc35ff6b4d994bbb760d19909fcdccc349c
Benny <132187364+Benny-17@users.noreply.github.com>
2025-10-27 17:15:25 +0530

    automation-jenkins

---
+++ app-service/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h2>app</h2>
+  `);
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
+
